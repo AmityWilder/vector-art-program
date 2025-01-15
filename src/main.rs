8f0de7f3eb86ab459b8009305307daa698f76e04
Henry Wilder <henrythepony@gmail.com>
2025-01-14 20:04:52 -0500

    Extracted draw and iteration of layer tree into own function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, collections::BTreeMap, rc::Rc};
+use std::{cell::RefCell, collections::BTreeMap, rc::Rc, sync::OnceLock};
 use raylib::prelude::*;
 use rand::{distributions::Uniform, prelude::*};
 
@@ -239,7 +239,7 @@ impl Bitmap {
 }
 
 pub enum LayerItem {
-    Group(Rc<Layer>),
+    Group(Rc<RefCell<Layer>>),
     Path(Rc<RefCell<VectorPath>>),
     Bitmap(Bitmap),
 }
@@ -262,6 +262,27 @@ pub struct Layer {
     pub items: Vec<Rc<RefCell<LayerItem>>>,
 }
 
+pub enum LayerPanelTreeItemData<'a> {
+    Layer {
+        slot: Rectangle,
+        color_rec: Rectangle,
+        thumbnail_rec: Rectangle,
+        name_rec: Rectangle,
+        expand_collapse_rec: Rectangle,
+        layer: &'a mut Layer,
+    },
+    Path {
+        slot: Rectangle,
+        thumbnail_rec: Rectangle,
+        path: &'a mut Rc<RefCell<VectorPath>>,
+    },
+    Bitmap {
+        slot: Rectangle,
+        thumbnail_rec: Rectangle,
+        bitmap: &'a mut Bitmap,
+    },
+}
+
 impl Layer {
     pub fn new(name: String, color: Color) -> Self {
         Self {
@@ -281,7 +302,7 @@ impl Layer {
         if !self.is_hidden {
             for item in &self.items {
                 match &*item.borrow() {
-                    LayerItem::Group(group) => group.draw(d),
+                    LayerItem::Group(group) => group.borrow().draw(d),
                     LayerItem::Path(path) => path.borrow().draw(d, self.color),
                     LayerItem::Bitmap(bitmap) => bitmap.draw(d),
                 }
@@ -289,52 +310,111 @@ impl Layer {
         }
     }
 
-    pub fn draw_layer_tree(&self, d: &mut impl RaylibDraw, y: &mut f32, x: f32, width: f32) {
-        const GAP: f32 = 2.0;
-        const INDENT: f32 = 6.0;
-        const THUMBNAIL_SIZE: f32 = 32.0;
-        const THUMBNAIL_INSET: f32 = 6.0;
-        const LAYER_HEIGHT: f32 = 2.0 * THUMBNAIL_INSET + THUMBNAIL_SIZE;
-        const LAYER_COLOR_WIDTH: f32 = 4.0;
+    pub const GAP: f32 = 2.0;
+    pub const INDENT: f32 = 6.0;
+    pub const THUMBNAIL_SIZE: f32 = 32.0;
+    pub const THUMBNAIL_INSET: f32 = 6.0;
+    pub const LAYER_HEIGHT: f32 = 2.0 * Self::THUMBNAIL_INSET + Self::THUMBNAIL_SIZE;
+    pub const LAYER_COLOR_WIDTH: f32 = 4.0;
 
-        let mut rec = Rectangle::new(x, *y, width, LAYER_HEIGHT);
+    /// Calls the closure on each visible item in the layer panel tree until `f` returns [`Some`], then returns the output of `f`.
+    /// Returns [`None`] if `f` never returns [`Some`].
+    pub fn for_each_layer_tree_item_internal<T>(
+        &mut self,
+        panel: &Rectangle,
+        y: &mut f32,
+        x: f32,
+        width: f32,
+        f: &mut impl FnMut(LayerPanelTreeItemData<'_>) -> Option<T>
+    ) -> Option<T> {
+        {
+            let mut rec = Rectangle::new(x, *y, width, Self::LAYER_HEIGHT);
+            if rec.check_collision_recs(panel) {
+                let data = LayerPanelTreeItemData::Layer {
+                    slot: rec,
+                    color_rec: {
+                        rec.width = Self::LAYER_COLOR_WIDTH;
+                        rec
+                    },
+                    thumbnail_rec: {
+                        rec.x += Self::LAYER_COLOR_WIDTH + Self::THUMBNAIL_INSET;
+                        rec.y += Self::THUMBNAIL_INSET;
+                        (rec.width, rec.height) = (Self::THUMBNAIL_SIZE, Self::THUMBNAIL_SIZE);
+                        rec
+                    },
+                    name_rec: {
+                        rec.x += Self::THUMBNAIL_SIZE + Self::THUMBNAIL_INSET;
+                        rec.height = 10.0;
+                        rec.width = width - Self::LAYER_COLOR_WIDTH + Self::THUMBNAIL_INSET - Self::THUMBNAIL_SIZE;
+                        rec
+                    },
+                    expand_collapse_rec: {
+                        rec.y += 12.0;
+                        rec.width = 10.0;
+                        rec.height = 10.0;
+                        rec
+                    },
+                    layer: self,
+                };
 
-        // slot
-        d.draw_rectangle_rec(rec, Color::new(32,32,32,255));
+                let result = f(data);
+                if result.is_some() {
+                    return result;
+                }
+            }
 
-        // layer color
-        rec.width = LAYER_COLOR_WIDTH;
-        d.draw_rectangle_rec(rec, self.color);
-
-        // thumbnail
-        rec.x += LAYER_COLOR_WIDTH + THUMBNAIL_INSET;
-        rec.y += THUMBNAIL_INSET;
-        (rec.width, rec.height) = (THUMBNAIL_SIZE, THUMBNAIL_SIZE);
-        d.draw_rectangle_rec(rec, Color::GRAY);
-
-        // name
-        rec.x += THUMBNAIL_SIZE + THUMBNAIL_INSET;
-        d.draw_text(&self.name, rec.x as i32, rec.y as i32, 10, Color::new(200,200,200,255));
-
-        *y += LAYER_HEIGHT + GAP;
+            *y += Self::LAYER_HEIGHT + Self::GAP;
+        }
 
         if self.is_expanded {
             for item in self.items.iter().rev() {
-                match &*item.borrow() {
+                let item = &mut *item.borrow_mut();
+                match item {
                     LayerItem::Group(group) => {
-                        group.draw_layer_tree(d, y, x + INDENT, width - INDENT);
+                        let result = group.borrow_mut().for_each_layer_tree_item_internal(panel, y, x + Self::INDENT, width - Self::INDENT, f);
+                        if result.is_some() {
+                            return result;
+                        }
                     }
-                    LayerItem::Path(_path) => {
-                        // todo
-                        *y += LAYER_HEIGHT + GAP;
+
+                    LayerItem::Path(path) => {
+                        let mut rec = Rectangle::new(x, *y, width, Self::LAYER_HEIGHT);
+                        if rec.check_collision_recs(panel) {
+                            f(LayerPanelTreeItemData::Path {
+                                slot: rec,
+                                thumbnail_rec: {
+                                    rec.x += Self::LAYER_COLOR_WIDTH + Self::THUMBNAIL_INSET;
+                                    rec.y += Self::THUMBNAIL_INSET;
+                                    (rec.width, rec.height) = (Self::THUMBNAIL_SIZE, Self::THUMBNAIL_SIZE);
+                                    rec
+                                },
+                                path,
+                            });
+                        }
+                        *y += Self::LAYER_HEIGHT + Self::GAP;
                     }
-                    LayerItem::Bitmap(_bitmap) => {
-                        // todo
-                        *y += LAYER_HEIGHT + GAP;
+
+                    LayerItem::Bitmap(bitmap) => {
+                        let mut rec = Rectangle::new(x, *y, width, Self::LAYER_HEIGHT);
+                        if rec.check_collision_recs(panel) {
+                            f(LayerPanelTreeItemData::Bitmap {
+                                slot: rec,
+                                thumbnail_rec: {
+                                    rec.x += Self::LAYER_COLOR_WIDTH + Self::THUMBNAIL_INSET;
+                                    rec.y += Self::THUMBNAIL_INSET;
+                                    (rec.width, rec.height) = (Self::THUMBNAIL_SIZE, Self::THUMBNAIL_SIZE);
+                                    rec
+                                },
+                                bitmap,
+                            });
+                        }
+                        *y += Self::LAYER_HEIGHT + Self::GAP;
                     }
                 }
             }
         }
+
+        None
     }
 }
 
@@ -372,6 +452,81 @@ impl Document {
             art_boards: vec![ArtBoard::new("artboard 0".to_string(), rrect(0.0, 0.0, width, height))],
         }
     }
+
+    pub fn for_each_layer_tree_item<T>(&mut self, panel: &Rectangle, mut f: impl FnMut(LayerPanelTreeItemData) -> Option<T>) -> Option<T> {
+        const INSET: f32 = 2.0;
+
+        let mut y = panel.y + INSET;
+        let x = panel.x + INSET;
+        let width = panel.width - INSET * 2.0;
+
+        for layer in self.layers.iter_mut().rev() {
+            let result = layer.for_each_layer_tree_item_internal(panel, &mut y, x, width, &mut f);
+            if result.is_some() { return result; }
+        }
+
+        None
+    }
+
+    pub fn draw_layer_tree(&mut self, d: &mut impl RaylibDraw, panel: &Rectangle) {
+        let mut d = d.begin_scissor_mode(panel.x as i32, panel.y as i32, panel.width as i32, panel.height as i32);
+        d.draw_rectangle_rec(*panel, Color::new(24,24,24,255));
+        self.for_each_layer_tree_item(panel, |data| -> Option<()> {
+            match data {
+                LayerPanelTreeItemData::Layer {
+                    slot,
+                    color_rec,
+                    thumbnail_rec,
+                    name_rec,
+                    expand_collapse_rec,
+                    layer,
+                } => {
+                    d.draw_rectangle_rec(slot, Color::new(32,32,32,255));
+                    d.draw_rectangle_rec(color_rec, layer.color);
+                    d.draw_rectangle_rec(thumbnail_rec, Color::GRAY);
+                    d.draw_text(&layer.name, name_rec.x as i32, name_rec.y as i32, 10, Color::new(200,200,200,255));
+
+                    // expand icon
+                    {
+                        let [p0, p1, p2] = if layer.is_expanded {
+                            [
+                                Vector2::new(expand_collapse_rec.x, expand_collapse_rec.y),
+                                Vector2::new(expand_collapse_rec.x + 5.0, expand_collapse_rec.y + 6.0),
+                                Vector2::new(expand_collapse_rec.x + expand_collapse_rec.height, expand_collapse_rec.y),
+                            ]
+                        } else {
+                            [
+                                Vector2::new(expand_collapse_rec.x, expand_collapse_rec.y),
+                                Vector2::new(expand_collapse_rec.x, expand_collapse_rec.y + expand_collapse_rec.height),
+                                Vector2::new(expand_collapse_rec.x + 6.0, expand_collapse_rec.y + 5.0),
+                            ]
+                        };
+                        d.draw_triangle(p0, p1, p2, Color::new(200,200,200,255));
+                    }
+                }
+
+                LayerPanelTreeItemData::Path {
+                    slot,
+                    thumbnail_rec,
+                    path: _,
+                } => {
+                    d.draw_rectangle_rec(slot, Color::new(32,32,32,255));
+                    d.draw_rectangle_rec(thumbnail_rec, Color::GRAY);
+                }
+
+                LayerPanelTreeItemData::Bitmap {
+                    slot,
+                    thumbnail_rec,
+                    bitmap: _,
+                } => {
+                    d.draw_rectangle_rec(slot, Color::new(32,32,32,255));
+                    d.draw_rectangle_rec(thumbnail_rec, Color::GRAY);
+                }
+            }
+
+            None
+        });
+    }
 }
 
 pub enum Tool {
@@ -389,6 +544,11 @@ pub enum Tool {
 }
 
 fn main() {
+    let background_color: Color = Color::new(32,32,32,255);
+
+    let mut rng = thread_rng();
+    let uniform_u8 = Uniform::<u8>::new_inclusive(0, 255);
+
     let (mut rl, thread) = init()
         .title("Amity Vector Art")
         .size(1280, 720)
@@ -396,11 +556,6 @@ fn main() {
         .build();
 
     rl.set_target_fps(60);
-
-    let mut rng = thread_rng();
-    let uniform_u8 = Uniform::<u8>::new_inclusive(0, 255);
-
-    let background_color: Color = Color::new(32,32,32,255);
 
     let mut document = Document::new("untitled".to_string(), 256.0, 256.0);
     document.camera.target = Vector2::new(
@@ -465,45 +620,72 @@ fn main() {
             };
         }
 
-        match &mut current_tool {
-            Tool::DirectSelection { selection: _ } => {
-                // todo
-            }
-
-            Tool::Pen { current_path, current_anchor } => {
-                if rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) {
-                    if current_path.is_none() {
-                        // create a new path
-                        let path = VectorPath::new();
-                        let path = Rc::new(RefCell::new(path));
-                        document.layers.push({
-                            let mut layer = Layer::new(
-                                format!("layer {}", document.layers.len()),
-                                Color::new(
-                                    uniform_u8.sample(&mut rng),
-                                    uniform_u8.sample(&mut rng),
-                                    uniform_u8.sample(&mut rng),
-                                    255,
-                                ),
-                            );
-                            layer.items.push(Rc::new(RefCell::new(LayerItem::Path(path.clone()))));
-                            layer
-                        });
-                        *current_path = Some(path);
-                    }
-                    *current_anchor = Some(mouse_world_pos);
-                } else if rl.is_mouse_button_released(MouseButton::MOUSE_BUTTON_LEFT) {
-                    if let Some(current_path) = current_path.as_mut() {
-                        let mut path = current_path.borrow_mut();
-                        if let Some(anchor) = current_anchor.take() {
-                            path.points.push((
-                                anchor * 2.0 - mouse_world_pos, // x - (a - x) = 2x - a
-                                anchor,
-                                mouse_world_pos,
-                            ));
+        if layers_panel.check_collision_point_rec(mouse_screen_pos) {
+            if rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) {
+                document.for_each_layer_tree_item(&layers_panel, |data| -> Option<()> {
+                    match data {
+                        LayerPanelTreeItemData::Layer {
+                            slot,
+                            color_rec: _,
+                            thumbnail_rec: _,
+                            name_rec,
+                            expand_collapse_rec,
+                            layer,
+                        } if slot.check_collision_point_rec(mouse_screen_pos) => {
+                            if expand_collapse_rec.check_collision_point_rec(mouse_screen_pos) {
+                                layer.is_expanded = !layer.is_expanded;
+                            } else if name_rec.check_collision_point_rec(mouse_screen_pos) {
+                                println!("clicked name");
+                            } else {
+                                println!("clicked slot");
+                            }
+                            Some(())
                         }
-                    } else {
-                        println!("warning: `current_path` should have been set when mouse was pressed");
+                        _ => None,
+                    }
+                });
+            }
+        } else {
+            match &mut current_tool {
+                Tool::DirectSelection { selection: _ } => {
+                    // todo
+                }
+
+                Tool::Pen { current_path, current_anchor } => {
+                    if rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) {
+                        if current_path.is_none() {
+                            // create a new path
+                            let path = VectorPath::new();
+                            let path = Rc::new(RefCell::new(path));
+                            document.layers.push({
+                                let mut layer = Layer::new(
+                                    format!("layer {}", document.layers.len()),
+                                    Color::new(
+                                        uniform_u8.sample(&mut rng),
+                                        uniform_u8.sample(&mut rng),
+                                        uniform_u8.sample(&mut rng),
+                                        255,
+                                    ),
+                                );
+                                layer.items.push(Rc::new(RefCell::new(LayerItem::Path(path.clone()))));
+                                layer
+                            });
+                            *current_path = Some(path);
+                        }
+                        *current_anchor = Some(mouse_world_pos);
+                    } else if rl.is_mouse_button_released(MouseButton::MOUSE_BUTTON_LEFT) {
+                        if let Some(current_path) = current_path.as_mut() {
+                            let mut path = current_path.borrow_mut();
+                            if let Some(anchor) = current_anchor.take() {
+                                path.points.push((
+                                    anchor * 2.0 - mouse_world_pos, // x - (a - x) = 2x - a
+                                    anchor,
+                                    mouse_world_pos,
+                                ));
+                            }
+                        } else {
+                            println!("warning: `current_path` should have been set when mouse was pressed");
+                        }
                     }
                 }
             }
@@ -569,19 +751,7 @@ fn main() {
             }
 
             // Draw layers panel
-            {
-                d.draw_rectangle_rec(layers_panel, Color::new(24,24,24,255));
-
-                const INSET: f32 = 2.0;
-
-                let mut y = layers_panel.y + INSET;
-                let x = layers_panel.x + INSET;
-                let width = layers_panel.width - INSET * 2.0;
-
-                for layer in document.layers.iter().rev() {
-                    layer.draw_layer_tree(&mut d, &mut y, x, width);
-                }
-            }
+            document.draw_layer_tree(&mut d, &layers_panel);
         }
 
         mouse_screen_pos_prev = mouse_screen_pos;
